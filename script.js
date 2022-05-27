6cbec1145d5c76e97d94bdaafa0193404252daad
mouhcine <mouhcine15.00@gmail.com>
2022-05-27 17:54:40 +0200

    milestone 1

---
+++ script.js
@@ -0,0 +1,130 @@
+const root = new Vue({
+    el: "#app",
+    data: {
+        contacts: [
+            {
+                name: 'Zoro',
+                avatar: 'https://i.pinimg.com/736x/e6/67/3f/e6673fc4f4930a84277c3f88bc87978e.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Sanji',
+                avatar: 'https://i.pinimg.com/736x/50/c7/eb/50c7eb22dd5a72f8319223760c8c023f.jpg',
+                visible: false,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        message: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        message: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Robin',
+                avatar: 'https://i.pinimg.com/originals/3b/bd/41/3bbd41b7d80560b84ca0ef26300f41fb.jpg',
+                visible: false,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        message: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        message: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        message: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Jimbei',
+                avatar: 'https://i.pinimg.com/736x/23/96/43/239643b6fcd730c95a5019fdc642b5c6.jpg',
+                visible: false,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Usopp',
+                avatar: 'https://i.pinimg.com/originals/d2/80/b3/d280b334747451a0de6a4a7624418793.jpg',
+                visible: false,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ricordati di chiamare la nonna',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Va bene, stasera la sento',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Brook',
+                avatar: 'https://i.pinimg.com/originals/bf/95/5b/bf955b22263954ce65a26f0fc58360c7.jpg',
+                visible: false,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ciao Claudia, hai novità?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Non ancora',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:51:00',
+                        message: 'Nessuna nuova, buona nuova',
+                        status: 'sent'
+                    }
+                ],
+            }
+            
+        ] 
+    }
+});
